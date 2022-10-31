aa4d230dcfa37edad8ff8cc8c225dcebb14c9b93
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2022-10-31 00:45:38 -0300

    Backdoor

---
+++ .cs
@@ -0,0 +1,283 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Proxies;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Configuration;
+using System.Xml.XPath;
+using static System.Net.WebRequestMethods;
+
+namespace VisualRun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+internal class Executor
+{
+    private object executar;
+
+    public Executor()
+    {
+        var open = ("class");
+            var executar1 = executar;
+    }
+}
+class OpenBackupEventLog
+{
+    public OpenBackupEventLog(OpenBackupEventLog openBackupEventLog)
+    {
+        object value = ("class");
+    }
+}
+class IMPORT_OBJECT_CODE
+{
+    private IMPORT_OBJECT_CODE https;
+
+    public IMPORT_OBJECT_CODE()
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+};
+
+internal class DefaultProxySection
+{
+    public DefaultProxySection(object https)
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+}
+
+class DebuggerTypeProxyAttribute
+{
+    public DebuggerTypeProxyAttribute()
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+
+    public object https { get; private set; }
+}
+
+class IWebProxy
+{
+    public IWebProxy()
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+
+    public object https { get; }
+}
+
+class AssemblyNameProxy
+{
+    public AssemblyNameProxy()
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+}
+
+class ProxyAttribute
+{
+    public ProxyAttribute()
+    {
+        var url = ("FA:8F:CA:55:A0:F4");
+    }
+}
+
+class ParameterizedThreadStart
+{
+    public ParameterizedThreadStart()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.exe");
+    }
+    import parameterizedthreadstart = ("CodadoVisual.html");
+}
+
+class DllImportSearchPath
+{
+    public DllImportSearchPath()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.exe");
+    }
+    import parameterizedthreadstart = ("CodadoVisual.html");
+}
+
+class RootedPathValidator
+{
+    public RootedPathValidator()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.exe");
+    }
+    import parameterizedthreadstart = ("CodadoVisual.html");
+}
+
+class PathType
+{
+    public PathType()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.exe");
+    }
+    import parameterizedthreadstart = ("CodadoVisual.html");
+}
+
+class PathTooLongException
+{
+    public PathTooLongException()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.html");
+    }
+    import parameterizedthreadstart = ("Run.40");
+}
+class CallerFilePathAttribute
+{
+    public CallerFilePathAttribute()
+    {
+        var parameterizedthreadstart = ("CodadoVisual.exe");
+    }
+    import parameterizedthreadstart = ("Run.40");
+}
+#pragma warning disable CS0168 // Variable is declared but never used
+class InterfaceMapping
+{
+    public InterfaceMapping()
+    {
+        var InvalidOleVariantTypeException = new InvalidOperationException("class");
+    }
+}
+#pragma warning restore CS0168 // Variable is declared but never used
+class ClaimValueTypes
+{
+    public ClaimValueTypes()
+    {
+        var parameterizedthreadstart = ("Run10.exe");
+    }
+    import parameterizedthreadstart = ("Run.20");
+
+}
+class WebInvasao
+{
+    import parameterizedthreadstart = ("Run.40.html");
+}
+
+[DebuggerDisplay("{" + nameof(DebuggerDisplay) + "(),nq}")]
+class main
+{
+    private string DebuggerDisplay { get; }
+
+    class ParameterizedThreadStart
+    {
+        public ParameterizedThreadStart()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+
+    class DllImportSearchPath
+    {
+        public DllImportSearchPath()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+
+    class RootedPathValidator
+    {
+        public RootedPathValidator()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+
+    class PathType
+    {
+        public PathType()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+
+    class PathTooLongException
+    {
+        public PathTooLongException()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+    class CallerFilePathAttribute
+    {
+        public CallerFilePathAttribute()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+    }
+#pragma warning disable CS0168 // Variable is declared but never used
+    class InterfaceMapping
+    {
+        public InterfaceMapping()
+        {
+            var InvalidOleVariantTypeException = new InvalidOperationException("class");
+        }
+    }
+#pragma warning restore CS0168 // Variable is declared but never used
+    class ClaimValueTypes
+    {
+        public ClaimValueTypes()
+        {
+            var parameterizedthreadstart = ("Run10.exe");
+        }
+        import parameterizedthreadstart = ("Run.20");
+
+    }
+    class WebInvasao
+    {
+        import parameterizedthreadstart = ("Run.40.html");
+    }
+
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    class Codado
+    {
+        private string GetDebuggerDisplay()
+        {
+            import parameterizedthreadstart = ("CodadoVisual.exe");
+            return ToString();
+        }
+        import parameterizedthreadstart = ("CodadoVisual.html");
+
+    }
+    class XPathDocument
+    {
+        public XPathDocument()
+        {
+            var OpenBackupEventLog = ("J:\\Backup\\Backup\\Backup");
+        }
+    }
+    class SqlClientLogger
+    {
+        public SqlClientLogger()
+        {
+            var OpenBackupEventLog = ("J:\\Backup\\Backup\\Backup");
+        }
+    }
+}
